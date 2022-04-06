976f1bcd2d521afcca7048f1db55ce89cba3a660
Andrei Rahalevich <https://github.com/Andrei199729/Prech.git>
2022-04-06 20:03:11 +0300

    test1

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { errors } = require('celebrate');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routerErrorWay = require('./routes/errorsway');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
+const { registerValid, loginValid } = require('./middlewares/validationJoi');
+const { requestLogger, errorLoger } = require('./middlewares/logger');
+
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(requestLogger);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signup', registerValid, createUser);
+app.post('/signin', loginValid, login);
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
+
+app.use('/cards', require('./routes/cards'));
+
+app.use(errorLoger);
+
+app.use(auth);
+
+app.use(routerErrorWay);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+app.listen(PORT);
